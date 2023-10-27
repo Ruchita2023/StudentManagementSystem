857db9ec6511cf2871f9ba985ae206f101eaec7d
root <root@ip-172-31-42-126.ap-south-1.compute.internal>
2023-10-27 08:17:32 +0000

    TestGen generated unit tests - 2023-10-27 08:17

--- python-flask-server/openapi_server/test/test_user_service.py
+++ python-flask-server/openapi_server/test/test_user_service.py
@@ -2,35 +2,47 @@ from services.user_service import *
 import unittest
 from openapi_server.models.user import User
 from openapi_server.config_test import db
-from openapi_server.service import UserService
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 class TestUserService(unittest.TestCase):
 
     def test_create_user(self):
-        """
-        Test creating a new user
-        """
-        user = UserService.create_user('test_user', 'test_pass')
+        # Test creating a user
+        user = UserService.create_user("test_user", "test_password")
         self.assertIsInstance(user, User)
-        self.assertEqual(user.username, 'test_user')
-        self.assertEqual(user.password, 'test_pass')
+        self.assertEqual(user.username, "test_user")
+        self.assertEqual(user.password, "test_password")
 
     def test_get_user_by_username(self):
-        """
-        Test getting a user by their username
-        """
-        UserService.create_user('test_user', 'test_pass')
-        user = UserService.get_user_by_username('test_user')
+        # Test getting a user by username
+        UserService.create_user("test_user", "test_password")
+        user = UserService.get_user_by_username("test_user")
         self.assertIsInstance(user, User)
-        self.assertEqual(user.username, 'test_user')
-        self.assertEqual(user.password, 'test_pass')
+        self.assertEqual(user.username, "test_user")
+        self.assertEqual(user.password, "test_password")
 
-    def test_get_user_by_invalid_username(self):
-        """
-        Test getting a user with an invalid username
-        """
-        user = UserService.get_user_by_username('invalid_username')
+    def test_get_user_by_nonexistent_username(self):
+        # Test getting a user by a nonexistent username
+        user = UserService.get_user_by_username("nonexistent_user")
         self.assertIsNone(user)
+
+    def test_create_duplicate_user(self):
+        # Test creating a user with a duplicate username
+        UserService.create_user("test_user", "test_password")
+        with self.assertRaises(Exception):
+            UserService.create_user("test_user", "test_password")
+
+    def test_create_user_with_empty_username(self):
+        # Test creating a user with an empty username
+        with self.assertRaises(Exception):
+            UserService.create_user("", "test_password")
+
+    def test_create_user_with_empty_password(self):
+        # Test creating a user with an empty password
+        with self.assertRaises(Exception):
+            UserService.create_user("test_user", "")
 
 if __name__ == '__main__':
     unittest.main()
